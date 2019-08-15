8be829fc7b7efced134ab5c13b0b6c8b73497cb3
github username <github email address>
2019-08-15 17:40:58 -0400

    added users endpoint in backend

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+    def index 
+        users = User.all 
+        render json: users 
+    end 
+
+    def create 
+
+    end 
+
+    def show 
+        user = User.find(params[:id])
+        render json: user
+    end 
+
+    def update 
+
+    end 
+
+    def destroy 
+
+    end 
+
+
+
 end
