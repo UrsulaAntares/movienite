66e29dd723cf0d0d11441f9a0d2b14ddfe9bdf16
github username <github email address>
2019-08-15 17:25:30 -0400

    basic seeds of models for skateboarding

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+movies = Movie.create([ {title: "Mars Attacks!"}, {title: "Hudsucker Proxy"}]  )
+users = User.create([{name: "Ursula", username: "Urs"}, {name: "Kolton", username: "KStarr"},{name: "Frank", username: "frank"},{name: "Angela", username: "Angie"}])
+nights = Night.create([{name:"We have a projector"}, {name: "We have a couch"}, {name: "Matt's movie night"}])
+
+10.times  {Interest.create(heart: rand(0..5), star: rand(0..5), user_id: User.all.sample.id, movie: Movie.all.sample ) }
+
+10.times  {Join.create([user: User.all.sample, night: Night.all.sample])}
+
+
