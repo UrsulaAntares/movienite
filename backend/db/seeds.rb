0d6c45f9d5a60bebf0b2c26a1c7e79955faae48d
github username <github email address>
2019-08-16 16:31:06 -0400

    Closer

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,27 +1,27 @@
-require 'rest-client'
+# require 'rest-client'
 require 'json'
-require 'pry'
+# require 'pry'
 
 API_BASE_URL="https://api.themoviedb.org/3/trending/all/day?api_key=d58a8cfa0b1932711fc5fc8dda7ee621"
 
-def get_movies()
-    response = RestClient.get(API_BASE_URL)
-    json = JSON.parse(response.body)
-    json.map do |movie|
-        {
+# def get_movies()
+#     response = RestClient.get(API_BASE_URL)
+#     json = JSON.parse(response.body)
+#     json.map do |movie|
+#         {
             
-        } 
-end
+#         } 
+# end
 
 
 
 
-movies = Movie.create([ {title: "Mars Attacks!"}, {title: "Hudsucker Proxy"}]  )
-users = User.create([{name: "Ursula", username: "Urs"}, {name: "Kolton", username: "KStarr"},{name: "Frank", username: "frank"},{name: "Angela", username: "Angie"}])
-nights = Night.create([{name:"We have a projector"}, {name: "We have a couch"}, {name: "Matt's movie night"}])
+# movies = Movie.create([ {title: "Mars Attacks!"}, {title: "Hudsucker Proxy"}]  )
+# users = User.create([{name: "Ursula", username: "Urs"}, {name: "Kolton", username: "KStarr"},{name: "Frank", username: "frank"},{name: "Angela", username: "Angie"}])
+# nights = Night.create([{name:"We have a projector"}, {name: "We have a couch"}, {name: "Matt's movie night"}])
 
-10.times  {Interest.create(heart: rand(0..5), star: rand(0..5), user_id: User.all.sample.id, movie: Movie.all.sample ) }
+# 10.times  {Interest.create(heart: rand(0..5), star: rand(0..5), user_id: User.all.sample.id, movie: Movie.all.sample ) }
 
-10.times  {Join.create([user: User.all.sample, night: Night.all.sample])}
+# 10.times  {Join.create([user: User.all.sample, night: Night.all.sample])}
 
-
+moremovies = Movie.create([ {title: "Birdman"}, {title: "It's a Wonderful Life"}, {title: "Best in Show"}]  )
