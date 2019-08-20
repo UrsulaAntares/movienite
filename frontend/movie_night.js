87ad31d92d077c64cb4d9a84e03e992c07208267
github username <github email address>
2019-08-20 14:20:56 -0400

    User can create new movie night and add other users

--- frontend/movie_night.js
+++ frontend/movie_night.js
@@ -10,8 +10,8 @@ function movieNight(e, user, movieContainer){
                 <img class="uk-border-circle" width="40" height="40" src="images/little-film.png">
             </div>
             <div class="uk-width-expand">
-                <h3 class="uk-card-title uk-margin-remove-bottom">${user.simple_user_data.name}'s Movie Night</h3>
-                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+                <h3 class="uk-card-title uk-margin-remove-bottom">Create Movie Night</h3>
+                <p class="uk-text-meta uk-margin-remove-top">Will take place on: <time datetime="2016-04-01T19:00">April 01, 2016</time></p>
             </div>
         </div>
     </div>
@@ -20,7 +20,7 @@ function movieNight(e, user, movieContainer){
         
         <form class="uk-search uk-search-default" id="add_user">
             <input class="uk-search-input" type="search" placeholder="Name your movie night...">
-            <input id="users" class="uk-search-input" type="search" placeholder="Start with a friend...">
+            <input id="users" class="uk-search-input" type="datetime-local">
             <input type="submit">
         </form>
         
@@ -47,17 +47,55 @@ let userForm = document.getElementById('add_user')
 }
 
 function createMovieNight(e, user, users_list){
+    e.preventDefault()
     let nightName = e.target[0].value.trim()
-    let addedUser = e.target[1].value.trim()
-    obj = {night: nightName, friend: addedUser, user: user}
+    let time = e.target[1].value
+    obj = {night: nightName, user: user}
     fetch("http://localhost:3000/nights", {
         method: "POST",
         headers: {'Content-Type':'application/json', 'Accept':'application/json'},
         body: JSON.stringify(obj)
     })
+    .then(r => r.json())
+    .then((data) => editPage(data, user, time))
     
     
 
 
 
+}
+
+function editPage(data, user, time){
+    let oldForm = document.getElementById('add_user')
+    let newForm = document.createElement('form')    
+        newForm.innerHTML = `
+        <div class="uk-margin">
+            <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                <input class="uk-input" type="text" placeholder="Add a User...">
+            </div>
+                <input type="submit">
+        </div>`
+        oldForm.parentNode.replaceChild(newForm, oldForm);
+
+        newForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            let newFriend = e.target[0].value.trim()
+
+            fetch("http://localhost:3000/add", {
+                method: "POST",
+                headers: {'Content-Type':'application/json', 'Accept':'application/json'},
+                body: JSON.stringify({newFriend: newFriend, data: data})
+            })
+            // .then(r => r.json()).then((data) => {debugger})
+        })
+
+        let cardTitle = document.getElementsByTagName('h3')[0]
+            cardTitle.innerText = `${data.night.name}`
+
+        
+        
+
+
+        // debugger
 }
